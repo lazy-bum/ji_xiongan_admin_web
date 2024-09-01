e02e012d306bc34ac11a55719049f11fd0271ab1
lazybum <lazybum_li@163.com>
2024-09-01 18:20:16 +0800

    feat: 删除部分路由

--- src/views/error/404.vue
+++ src/views/error/404.vue
@@ -3,7 +3,9 @@
     <div>
       <h1>404</h1>
       <h1>OOPS！你好像走丢了...</h1>
-      <RouterLink :to="{ path: '/', replace: true }" class="ant-btn ant-btn-primary"
+      <RouterLink
+        :to="{ path: '/dashboard/welcome', replace: true }"
+        class="ant-btn ant-btn-primary"
         >回到首页</RouterLink
       >
     </div>
